4bc71f105b3456f99b8afa3cd15fdc6234bdb1e8
Dennis Owusu <haledennis0@gmail.com>
2025-07-17 13:05:50 +0000

    feat: Add repositories for order

---
+++ apps/api/src/main/java/com/ftb/api/repository/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.ftb.api.repository;
+
+import com.ftb.api.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
+}
