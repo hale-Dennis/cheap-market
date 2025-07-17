6d4b8e75dcae55d0adb13a834cb368bb358ab2e4
Dennis Owusu <haledennis0@gmail.com>
2025-07-17 12:46:54 +0000

    feat: enable caching in main application class

--- apps/api/src/main/java/com/ftb/api/ApiApplication.java
+++ apps/api/src/main/java/com/ftb/api/ApiApplication.java
@@ -1,9 +1,12 @@
 package com.ftb.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableCaching
 public class ApiApplication {
 
 	public static void main(String[] args) {
