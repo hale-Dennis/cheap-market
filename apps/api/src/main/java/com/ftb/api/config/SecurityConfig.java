465cbf28b1ab538e42238054e28b54a60c9093b3
Dennis Owusu <haledennis0@gmail.com>
2025-07-18 11:23:36 +0000

    feat: Add swagger documentation for api

--- apps/api/src/main/java/com/ftb/api/config/SecurityConfig.java
+++ apps/api/src/main/java/com/ftb/api/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/v1/auth/**", "/api/v1/products/**").permitAll()
+                        req.requestMatchers("/api/v1/auth/**", "/api/v1/products/**", "/swagger-ui/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
